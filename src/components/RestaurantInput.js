fa1415422abc5dcb5a556e33382cbe52a6805e36
Vinny Martinez <vinny@vinnymartinez.com>
2017-05-29 16:19:35 -0400

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,36 +1,47 @@
 import React, { Component } from 'react';
-import { addRestaurant } from '../actions/restaurants'
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addRestaurant } from '../actions/restaurants';
 
 
 export class RestaurantInput extends Component {
-  constructor(props){
-    super(props)
-    this.state = {name: '', location: ''}
+  constructor(props) {
+    super(props);
+    this.state = {name: '', location: ''};
   }
-  handleOnNameChange(event){
-    this.setState({name: event.target.value})
+
+  handleOnNameChange(event) {
+    this.setState({name: event.target.value});
   }
-  handleOnLocationChange(event){
-    this.setState({location: event.target.value})
+
+  handleOnLocationChange(event) {
+    this.setState({location: event.target.value});
   }
-  handleOnSubmit(event){
-    event.preventDefault()
-    this.props.addRestaurant(this.state)
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addRestaurant(this.state);
   }
+
   render(){
-    return(
+    return (
       <form onSubmit={this.handleOnSubmit.bind(this)}>
-      <p>
-        <input type="text" onChange={this.handleOnNameChange.bind(this)} placeholder="restaurant name"/>
-      </p>
-      <p>
-        <input type="text" onChange={this.handleOnLocationChange.bind(this)} placeholder="location"/>
-      </p>
+        <p>
+          <input type="text" onChange={this.handleOnNameChange.bind(this)} placeholder="restaurant name"/>
+        </p>
+        <p>
+          <input type="text" onChange={this.handleOnLocationChange.bind(this)} placeholder="location"/>
+        </p>
         <input type="submit" />
       </form>
-    )
+    );
   }
 }
 
-export const ConnectedRestaurantInput = connect(null, null)(RestaurantInput)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    addRestaurant
+  }, dispatch);
+}
+
+export const ConnectedRestaurantInput = connect(null, mapDispatchToProps)(RestaurantInput);
--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
-import { Provider } from 'react-redux'
+import App from './App';
+import { Provider } from 'react-redux';
 
-
-import { createStore } from 'redux'
-import manageRestaurants from './reducers/manageRestaurants'
+import { createStore } from 'redux';
+import manageRestaurants from './reducers/manageRestaurants';
 // function(){}
 
 // we wrap store in a function for testing purposes
 export function configureStore(){
-  return createStore(manageRestaurants, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  return createStore(manageRestaurants
+    //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   )
 }
 
-let store = configureStore()
+let store = configureStore();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App store={store}/>
-  </Provider>
-    ,
-    document.getElementById('root')
-
-);
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App store={store}/>
+//   </Provider>,
+//   document.getElementById('root')
+// );
